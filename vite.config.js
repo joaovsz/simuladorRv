95b820d8a2dea4602da870952d472657e9f92d06
joaovsz <joaovitor1713coin@gmail.com>
2022-05-05 15:47:47 -0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/simuladorRV/",
+  base: " https://joaovsz.github.io/simuladorRV/",
   plugins: [react()]
 })
