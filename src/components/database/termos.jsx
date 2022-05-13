f16990aca2df544c39714c2a08058e6c1ebafdea
joaovsz <joaovitor1713coin@gmail.com>
2022-05-13 13:40:01 -0300

    BANCO DE TERMOS ATUALIZADO

---
+++ src/components/database/termos.jsx
@@ -0,0 +1,95 @@
+export const termos = [
+    {
+      name: "OiTv Novato",
+      taxa: {
+        Q1: 15.4,
+        Q2: 18.15,
+        Q3: 20.9,
+        Q4: 23.65,
+      },
+      multiplicadores: {
+        Q1: 1.5,
+        Q2: 0.94,
+        Q3: 0.75,
+        Q4: 0.56,
+      },
+      taxaIRC: {
+        Q1: 13.75,
+        Q2: 14.85,
+        Q3: 15.95,
+      },
+      irc: {
+        Q1: 0.2,
+        Q2: 0.5,
+        Q3: 0.9,
+      },
+      tma: {
+        Q1: 650,
+      },
+      deflateTma: 30,
+    },
+    {
+      name: "OiTv Veterano",
+
+      taxa: {
+        Q1: 14,
+        Q2: 16.5,
+        Q3: 19,
+        Q4: 21.5,
+      },
+
+      multiplicadores: {
+        Q1: 2,
+        Q2: 1.25,
+        Q3: 1,
+        Q4: 0.75,
+      },
+
+      taxaIRC: {
+        Q1: 12.5,
+        Q2: 13.5,
+        Q3: 14.5,
+      },
+      irc: {
+        Q1: 0.2,
+        Q2: 0.5,
+        Q3: 0.9,
+      },
+      tma: {
+        Q1: 620,
+      },
+      deflateTma: 30,
+    },
+    {
+      name: "OiTv Novato 04/2022",
+
+      taxa: {
+        Q1: 15.4,
+        Q2: 17.6,
+        Q3: 19.8,
+        Q4: 25.85,
+      },
+
+      multiplicadores: {
+        Q1: 1.5,
+        Q2: 0.94,
+        Q3: 0.75,
+        Q4: 0.56,
+      },
+
+      taxaIRC: {
+        Q1: 15.95,
+        Q2: 17.05,
+        Q3: 18.15,
+      },
+      irc: {
+        Q1: 0.2,
+        Q2: 0.5,
+        Q3: 0.9,
+      },
+      tma: {
+        Q1: 650,
+      },
+      deflateTma: 30,
+    },
+  ];
