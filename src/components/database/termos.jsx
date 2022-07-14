f10ce9c7140319e6bcbe5f285e6553399fc4e22f
joaovsz <joaovitor1713coin@gmail.com>
2022-07-14 10:03:14 -0300

    Novo Termo Mês 7

--- src/components/database/termos.jsx
+++ src/components/database/termos.jsx
@@ -17,7 +17,7 @@ export const termos = [
     },
 
     taxaIRC: {
-      Q1: 13.5,
+      Q1: 12.5,
       Q2: 14.5,
       Q3: 15.44,
     },
@@ -32,26 +32,26 @@ export const termos = [
     deflateTma: 0,
   },
   {
-    name: "OiTv Veterano 05/2022",
+    name: "OiTv Veterano 07/2022",
 
     taxa: {
-      Q1: 14,
-      Q2: 16.5,
-      Q3: 19,
-      Q4: 21.5,
+      Q1: 14.1,
+      Q2: 14.5,
+      Q3: 16.5,
+      Q4: 20,
     },
 
     multiplicadores: {
-      Q1: 2,
-      Q2: 1.25,
-      Q3: 1,
-      Q4: 0.75,
+      Q1: 3,
+      Q2: 2,
+      Q3: 1.5,
+      Q4: 1,
     },
 
     taxaIRC: {
-      Q1: 12.5,
-      Q2: 13.5,
-      Q3: 14.5,
+      Q1: 14,
+      Q2: 15,
+      Q3: 15.5,
     },
     irc: {
       Q1: 0.2,
@@ -59,7 +59,7 @@ export const termos = [
       Q3: 0.9,
     },
     tma: {
-      Q1: 620,
+      Q1: 590,
     },
     deflateTma: 30,
   },
